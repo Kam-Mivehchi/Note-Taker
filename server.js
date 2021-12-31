895205254d8ad46c39b3f4a3b90a545f6d120f22
Kamyar Mivehchi <kammivehchi@gmail.com>
2021-12-30 18:32:01 -0800

    first commit

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.get('/notes', (req,res)=>{
+    res.sendFile("./public/notes.html");
+});
 
 app.listen(PORT,function(){
     console.log("App listening on PORT:" + PORT);
