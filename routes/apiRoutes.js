97021d89607d7a905ee926aa456d25b390dab4e8
kmivehchi-xRoird <kmivehchi@instructors.2u.com>
2022-09-08 17:20:28 -0700

    convert api routes into async/await pattern

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,33 @@
 
 const router = require('express').Router();
 const storage = require('../models/Storage')
-// router.get('/', function (req, res) {
-//    res.sendFile(path.join(__dirname, '../public/notes.html'));
-// })
 
-router.get('/notes', function (req, res) {
-   storage
-      .getAll().then((notes) => {
-         return res.json(notes)
-      }).catch(e => res.status(500).json(e))
+
+router.get('/notes', async function (req, res) {
+   try {
+      let notes = await storage.getAll();
+      return res.json(notes)
+   } catch (e) {
+      res.status(500).json(e)
+   }
 })
-router.post('/notes', function (req, res) {
-
-   storage
-      .add(req.body).then((newNote) => {
-         return res.json(newNote)
-      }).catch(e => res.status(500).json(e))
+router.post('/notes', async function (req, res) {
+   try {
+      const newNote = await storage.add(req.body);
+      return res.json(newNote)
+   } catch (e) {
+      res.status(500).json(e)
+   }
 })
 
-router.delete('/notes/:id', (req, res) => {
-   storage
-      .remove(req.params.id)
-      .then(() => res.json(200))
-      .catch((e) => res.json(500).json(e))
+router.delete('/notes/:id', async (req, res) => {
+   try {
+      const remove = await storage.remove(req.params.id);
+      return remove
+   } catch (e) {
+      res.status(500).json(e)
+   }
+
 })
 
 module.exports = router
